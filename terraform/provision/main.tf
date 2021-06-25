b55705660ced03c0c5a643d8366fa02842b97832
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-06-25 15:55:19 -0700

    If domain was specified, output the necessary instructions and TXT and DKIM record content

--- terraform/provision/main.tf
+++ terraform/provision/main.tf
@@ -1,24 +1,52 @@
 locals {
   instance_id = "ses-${substr(sha256(var.instance_name), 0, 16)}"
-}
+  domain = (var.domain != "" ? var.domain : "${local.instance_id}.${var.default_domain}")
+  txt_verification_record = {
+    name    = "_amazonses"
+    type    = "TXT"
+    ttl     = "600"
+    records = [aws_ses_domain_identity.identity.verification_token]
+  }
 
-data "aws_route53_zone" "zone" {
-  name = var.domain
+  dkim_records = null_resource.dkim_records.*.triggers
+
+  # If no domain was specified, we need to create the records ourselves
+  route53_records = (var.domain != "" ? {} : 
+    {
+      # Old-style (SES v1) TXT verification record
+      txt_verification_record = local.txt_verification_record
+    }
+  )
+
+  instructions = (var.domain != "" ? "Your SMTP service is provisioned, but not verified. To verify the domain, create records in the ${var.domain} zone corresponding to the TXT and CNAME records provided in this output." :
+  null)
 }
 
 resource "aws_ses_domain_identity" "identity" {
-  domain = var.domain
+  domain = local.domain
 }
 
-resource "aws_route53_record" "record" {
-  zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "_amazonses.${local.instance_id}.${aws_ses_domain_identity.identity.id}."
-  type    = "TXT"
-  ttl     = "600"
-  records = [aws_ses_domain_identity.identity.verification_token]
+resource "aws_ses_domain_dkim" "dkim" {
+  domain = aws_ses_domain_identity.identity.domain
 }
 
-resource "aws_ses_domain_identity_verification" "verification" {
-  domain = aws_ses_domain_identity.identity.id
-  depends_on = [aws_route53_record.record]
+
+# We need to tell the caller about the three DKIM DNS records they must create.
+# There's no easy way to generate a nested structure in Terraform output, since
+# the for, for_each, count, and dynamic expressions don't work there. So we're
+# using this workaround:
+# https://gist.github.com/brikis98/f3fe2ae06f996b40b55eebcb74ed9a9e
+resource "null_resource" "dkim_records" {
+  count = 3
+
+  triggers = {
+    name     = format(
+        "%s._domainkey.%s",
+        element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index),
+        local.domain
+      )
+    type = "CNAME"
+    ttl  = "600"
+    records = "${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}.dkim.amazonses.com"
+  }
 }
--- terraform/provision/outputs.tf
+++ terraform/provision/outputs.tf
@@ -1,1 +1,13 @@
 output region { value = var.region }
+
+output verification_record {
+    value = var.domain != "" ? local.txt_verification_record : null
+}
+
+output dkim_records {
+    value = var.domain != "" ? local.dkim_records : null
+}
+
+output instructions {
+    value = local.instructions
+}

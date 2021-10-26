d772772ebc049b7dac16194abb518ee72252bda8
jbrown-xentity <jbrown@xentity.com>
2021-10-26 16:46:53 -0600

    Add test
    This adds checkdmarc to the test framework, and parses the output via jq

--- test.sh
+++ test.sh
@@ -4,6 +4,11 @@ set -e
 
 export SERVICE_INFO=$(echo "eden --client user --client-secret pass --url http://127.0.0.1:8080 credentials -b binding -i ${INSTANCE_NAME:-instance-${USER}}")
 
-echo "Running tests..."
+domain=`$SERVICE_INFO | jq -r '.domain_arn | split("/")[1]'`
 
-# None yet! Add some.
+echo "Running tests on ${domain}..."
+
+checkdmarc $domain | jq --exit-status '.dmarc.valid'
+
+# Would use this to test and get if spf record is valid
+# checkdmarc $domain | jq --exit-status '.spf.valid'

1d669c0150eb5849db863225df2a139feeb32c8d
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-10-27 11:52:16 -0400

    fix: data type of output record

--- terraform/provision/main.tf
+++ terraform/provision/main.tf
@@ -84,6 +84,6 @@ resource "null_resource" "dmarc_records" {
     name    = "_dmarc.${local.domain}"
     type    = "TXT"
     ttl     = "600"
-    records = ["v=DMARC1; p=quarantine; rua=mailto:${var.email_reciept_error}; ruf=mailto:${var.email_reciept_error}"]
+    records = "v=DMARC1; p=quarantine; rua=mailto:${var.email_reciept_error}; ruf=mailto:${var.email_reciept_error}"
   }
 }

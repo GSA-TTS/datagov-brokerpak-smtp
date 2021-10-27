6003bd2e944d2bf0bf237dffc92549351f9ff374
jbrown-xentity <jbrown@xentity.com>
2021-10-27 09:31:34 -0600

    See logged output of spf and dmarc
    Have ability to manually check records for verification from the logs...

--- test.sh
+++ test.sh
@@ -8,7 +8,11 @@ domain=`$SERVICE_INFO | jq -r '.domain_arn | split("/")[1]'`
 
 echo "Running tests on ${domain}..."
 
+echo "Is dmarc valid?"
+checkdmarc $domain | jq '.dmarc'
 checkdmarc $domain | jq --exit-status '.dmarc.valid'
 
 # Would use this to test and get if spf record is valid
+echo "Is spf valid?"
+checkdmarc $domain | jq '.spf'
 checkdmarc $domain | jq --exit-status '.spf.valid'
